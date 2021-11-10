2823fd68453f5d47ed929bb419dba1b993977f42
blackstarss <kota.weeds1204@gmail.com>
2021-11-10 08:10:39 +0000

    [start]StockDods

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
       resource :favorites , only: [:create , :destroy]
       resources :comments, only: [:create , :destroy]
     end
-    
+  
 
     resource :members do
       resource :relationships, only: [:create, :destroy]
