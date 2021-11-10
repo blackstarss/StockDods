5949c97caf7db0263c37f46f204a7d6190ca73c3
blackstarss <kota.weeds1204@gmail.com>
2021-11-10 01:14:15 +0000

    [finish]member,adminマイグレーション済

--- db/migrate/20211109150853_devise_create_admins.rb
+++ db/migrate/20211109150853_devise_create_admins.rb
@@ -31,6 +31,11 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
+      
+      t.string :name, null: false
+      t.string :image, null: false
+      t.text :biography, null: false
+      t.boolean :status, null: false, default: true
 
 
       t.timestamps null: false
