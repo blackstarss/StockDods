38c42225f1cf25a4273a6bd981f84c3d8c4bb591
blackstarss <kota.weeds1204@gmail.com>
2021-11-10 00:39:57 +0000

    [finish]devise設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  
+  #管理者用
+  devise_for :admin,controllers:{
+    sessions: 'admin/sessions'
+  }
+  #顧客用
+  devise_for :member,controllers:{
+    registrations: "member/registrations",
+    sessions: 'member/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
