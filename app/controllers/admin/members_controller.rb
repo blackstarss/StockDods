ff213823726178de92cf0d53591b73d379029f19
blackstarss <kota.weeds1204@gmail.com>
2021-11-12 02:32:55 +0000

    [start]adminトップページ＋member/show

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,2 +1,15 @@
 class Admin::MembersController < ApplicationController
+  
+  def show
+    @member = Member.find(params[:id])
+    @posts = @member.posts
+  end
+  
+  def edit
+  
+  end
+  
+  def update
+    
+  end
 end
