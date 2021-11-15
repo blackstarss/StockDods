8c5f85018e0d52b24a96ee801ae1a1d811e184ec
blackstarss <kota.weeds1204@gmail.com>
2021-11-15 03:49:36 +0000

    [update]post_hashtagsモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_034609) do
+ActiveRecord::Schema.define(version: 2021_11_15_034304) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
