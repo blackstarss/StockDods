3dd202fab1f86a9b22e4ca173083b341e2c06603
blackstarss <kota.weeds1204@gmail.com>
2021-11-22 05:03:47 +0000

    [start]フォローフォロアー機能

---
+++ app/controllers/member/relationships_controller.rb
@@ -0,0 +1,24 @@
+class Member::RelationshipsController < ApplicationController
+  
+# ——————フォロー機能を作成・保存・削除する————————————
+  def create
+    current_member.follow(params[:member_id])
+    redirect_to request.referer
+  end
+
+  def destroy
+    current_member.unfollow(params[:member_id])
+    redirect_to request.referer  
+  end
+#————————フォロー・フォロワー一覧を表示する-————————————
+  def followings
+    member = Member.find(params[:member_id])
+    @members = member.followings
+  end
+
+  def followers
+    member = Member.find(params[:member_id])
+    @members = member.followers
+  end
+  
+end
