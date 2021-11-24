ac69446b47ac23b084e0cd1dee8c2874e965ffdb
blackstarss <kota.weeds1204@gmail.com>
2021-11-24 13:07:54 +0000

    [update]refile key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd8d390b1d39026299736804521f078735cb4892a9ac934003ed6db5d9cc0caa8e4c96f9b8029e55366e44481401f310ee831c8ff8371f5e0ec89dbc46c3263df'
