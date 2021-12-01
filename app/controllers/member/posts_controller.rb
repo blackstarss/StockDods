a7fed295b67c77f560002392ce1f3b1319c29892
blackstarss <kota.weeds1204@gmail.com>
2021-12-01 00:51:27 +0000

    [update]いいねランキング

--- app/controllers/member/posts_controller.rb
+++ app/controllers/member/posts_controller.rb
@@ -8,14 +8,15 @@ class Member::PostsController < ApplicationController
   end
 
   def index
-    from = Time.now.at_beginning_of_day
-    to = (from + 6.day).at_end_of_day
-    posts = Post.includes(:favorites).
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    posts = Post.where(status: false).includes(:favorites).
       sort {|a,b|
         b.favorites.includes(:favorites).where(created_at: from...to).size <=>
         a.favorites.includes(:favorites).where(created_at: from...to).size
       }
     @posts = Kaminari.paginate_array(posts).page(params[:page])
+
     @member = current_member
     @genres = Genre.all
   end
